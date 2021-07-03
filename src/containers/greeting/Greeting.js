d0bf705c5272e4b72ae6968e3d20f4265702b4c2
S-codes14 <smlmnguni14@gmail.com>
2021-07-03 04:37:04 +0200

    cleaning up

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -18,58 +18,58 @@ export default function Greeting() {
   return (
     // <Fade bottom duration={1000} distance="40px">
 
-      <div className="greet-main" id="greeting">
-        <div className="greeting-main">
-    {/* <Fade left duration={1550}> */}
+    <div className="greet-main" id="greeting">
+      <div className="greeting-main">
+        {/* <Fade left duration={1550}> */}
 
-          <div className="greeting-text-div">
-            <div>
-              <h1
-                className={isDark ? "dark-mode greeting-text" : "greeting-text"}
-              >
-                {" "}
-                {"We ❤️ Problems"}{" "}
-                <span className="wave-emoji">{emoji("")}</span>
-              </h1>
-              <p
-                className={
-                  isDark
-                    ? "dark-mode greeting-text-p"
-                    : "greeting-text-p subTitle"
-                }
-              >
-                {"The problem is that teams compromise their brands’s potential with subpar Website, either because they don’t have a design, or don’t understand the vast complexities of developing.We believe that the ability to get stunning branded websites should not be reserved for the few, but should be democratized with no barrier to entry."}
-              </p>
-              {/* <SocialMedia /> */}
-              <div className="button-greeting-div">
-                {/* <Button text="Contact me" href="#contact" />
+        <div className="greeting-text-div">
+          <div>
+            <h1
+              className={isDark ? "dark-mode greeting-text" : "greeting-text"}
+            >
+              {" "}
+              {"We ❤️ Problems"} <span className="wave-emoji">{emoji("")}</span>
+            </h1>
+            <p
+              className={
+                isDark
+                  ? "dark-mode greeting-text-p"
+                  : "greeting-text-p subTitle"
+              }
+            >
+              {
+                "The problem is that teams compromise their brands’s potential with subpar Website, either because they don’t have a design, or don’t understand the vast complexities of developing.We believe that the ability to get stunning branded websites should not be reserved for the few, but should be democratized with no barrier to entry."
+              }
+            </p>
+            {/* <SocialMedia /> */}
+            <div className="button-greeting-div">
+              {/* <Button text="Contact me" href="#contact" />
                 <Button
                   text="See my resume"
                   newTab={true}
                   href={greeting.resumeLink}
                 /> */}
-              </div>
             </div>
           </div>
-    {/* </Fade> */}
-          
+        </div>
+        {/* </Fade> */}
 
         {/* <Fade left duration={1500}> */}
 
-          <div className="greeting-image-div">
-            {/* {illustration.animated ? (
+        <div className="greeting-image-div">
+          {/* {illustration.animated ? (
               <DisplayLottie animationData={landingPerson} />
             ) : ( */}
-              {/* <img
+          {/* <img
                 alt=""
                 src={require("../../assets/images/StarStuff.jpeg")}
               ></img> */}
-              <DisplayLottie animationData={Personal} />
+          <DisplayLottie animationData={Personal} />
 
-            {/* )} */}
-          </div>
-          {/* </Fade/> */}
+          {/* )} */}
         </div>
+        {/* </Fade/> */}
       </div>
+    </div>
   );
 }
